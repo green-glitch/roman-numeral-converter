eae5cf7ef6eea38fd532a504f92221ac05c8fa94
Krzysztof Kalinowski <kalinowskik234@gmail.com>
2025-02-19 09:51:48 +0100

    script.js file update

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
+const convertToRoman = (num) => {
+    const romanNumeral = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    let roman = '';
+
+    for (let key in romanNumeral) {
+        while (num >= romanNumeral[key]) {
+            roman += key;
+            num -= romanNumeral[key];
+        }
+    }
+
+    return roman;
+}
+
 convertBtn.addEventListener('click', () => {
     let romanNumeral = '';
     if (number.value === '') {
@@ -11,8 +40,8 @@ convertBtn.addEventListener('click', () => {
     }else if (number.value > 3999) {
         romanNumeral = 'Please enter a number less than or equal to 3999';
     } else {
-        //romanNumeral = convertToRoman(number.value);
-        romanNumeral = number.value;
+        romanNumeral = convertToRoman(number.value);
+        // romanNumeral = number.value;
     }
     output.innerHTML = romanNumeral;
 });
